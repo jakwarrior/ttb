8ff2cd9f003474d51e0b67591681f7a65bafca90
FLK <thefolken@online.fr>
2014-08-07 23:13:19 +0200

    New: Page spéciale en cas d'erreur.

--- index.php
+++ index.php
@@ -118,6 +118,9 @@ $f3=require('lib/base.php');
 $f3->config('config.ini');
 $f3->config('routes.ini');
 $f3->set('LANGUAGE', 'fr-FR');
+$f3->set('ONERROR',function($f3){
+  echo \Template::instance()->render('error.html');
+});
 $f3->run();
 
 ?>
