0d3bb44bc464fd5a2aa51f95ee0a8a4036ebaacd
FLK <thefolken@online.fr>
2014-08-14 21:31:25 +0200

    Change: Streams classés par "username"

--- app/controllers/StreamController.php
+++ app/controllers/StreamController.php
@@ -5,7 +5,7 @@ class StreamController extends Controller {
 	public function index()
     {
         $Streams = new Stream($this->db);
-        $this->f3->set('streams',$Streams->find());
+        $this->f3->set('streams',$Streams->find(NULL, array('order'=>'twitch_username ASC')));
         $this->f3->set('view','stream/list.htm');
 	}
 }
