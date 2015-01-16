eb6f81b198abfd48420f2d5f2149d90d2b83e1e9
FLKone <FLKone@users.noreply.github.com>
2015-01-16 18:47:50 +0100

    test v2 (multi col JS)

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -13,15 +13,15 @@ class HomeController extends Controller {
 
 
 
-        $this->f3->set('crs',$CRs->all(6));
+        $this->f3->set('crs',$CRs->all(20));
 
 		$crss = array (
-			array('username' => 'Zuukaa', 'games' => 'Stacraft 2', 'date' => '29/09@18h15', 'description' => 'Top Master@Europe'),
-			array('username' => 'Gracchus', 'games' => 'Dark Souls 2', 'date' => '30/09@21h00', 'description' => 'DLC, part.3'),
-			array('username' => 'bultom', 'games' => 'SimCity 2013', 'date' => '01/10@21h00', 'description' => 'Run sous contrainte'),
-			array('username' => 'Nightbringer57', 'games' => 'Banished', 'date' => '02/10@21h30', 'description' => 'Plus fort que le blabla, le TT, la sodogravier...'),
-			array('username' => 'bauer_attitude', 'games' => 'Dark Souls', 'date' => '03/10@21h00', 'description' => 'J\'ai Mal'),
-			array('username' => 'Ze_Fly', 'games' => 'Tower Of Guns', 'date' => '04/09@18h00', 'description' => 'L\'école du skill :o')
+			array('image' => 'http://image.jeuxvideo.com/images/pc/s/t/starcraft-ii-heart-of-the-swarm-pc-00b.jpg', 'username' => 'Zuukaa', 'games' => 'Stacraft 2', 'date' => '29/09@18h15', 'description' => 'Top Master@Europe'),
+			array('image' => 'http://farm6.staticflickr.com/5512/9674184368_51841e9b56.jpg', 'username' => 'Gracchus', 'games' => 'Dark Souls 2', 'date' => '30/09@21h00', 'description' => 'DLC, part.3'),
+			//array('image' => 'http://www.gamerobs.com/galerie/upload/data/8200147588b526f402ff2b591c3ffd49.jpg', 'username' => 'bultom', 'games' => 'SimCity 2013', 'date' => '01/10@21h00', 'description' => 'Run sous contrainte'),
+			array('image' => 'http://www.dlcompare.fr/img/banished-img-4.jpg', 'username' => 'Nightbringer57', 'games' => 'Banished', 'date' => '02/10@21h30', 'description' => 'Plus fort que le blabla, le TT, la sodogravier...'),
+			//array('image' => 'http://cdn.akamai.steamstatic.com/steam/apps/211420/header.jpg?t=1414091965', 'username' => 'bauer_attitude', 'games' => 'Dark Souls', 'date' => '03/10@21h00', 'description' => 'J\'ai Mal'),
+			array('image' => 'http://g3ar.co.za/wp-content/uploads/2014/02/Rouge-light-FPS-Tower-of-Guns-Comes-to-PC-on-March-4th-News-G3AR-600x300.jpg', 'username' => 'Ze_Fly', 'games' => 'Tower Of Guns', 'date' => '04/09@18h00', 'description' => 'L\'école du skill :o')
 		);
 
 		$this->f3->set('streams',$crss);
--- ui/js/ttb.js
+++ ui/js/ttb.js
@@ -19,44 +19,11 @@ $(window).scroll(function(e){
   parallax();
 });
 
-function stickIt() {
-
-  var orgElementPos = $('.original').offset();
-  var orgElementTop = orgElementPos.top;
-
-    if ($(window).scrollTop() >= (572 - 120)) {
-        $('#header').addClass('noshadow');
-
-    }
-    else {
-        $('#header').removeClass('noshadow');
-
-    }
-
-  if ($(window).scrollTop() >= (572 - 110)) {
-    // scrolled past the original position; now only show the cloned, sticky element.
-
-    // Cloned element should always have same left position and width as original element.
-    //orgElement = $('.original');
-    //coordsOrgElement = orgElement.offset();
-    //leftOrgElement = coordsOrgElement.left;
-    //widthOrgElement = orgElement.css('width');
-
-    $('.cloned').show();
-    $('.original').css('visibility','hidden');
-  } else {
-    // not scrolled past the menu; only show the original menu.
-    $('.cloned').hide();
-    $('.original').css('visibility','visible');
-
-  }
-}
-
-$(document).ready(function()
-{
-
-    // Create a clone of the menu, right next to original.
-    $('.header_menu').addClass('original').clone().insertAfter('.header_menu').addClass('cloned').removeClass('original').hide();
-
-    var scrollIntervalID = setInterval(stickIt, 10);
+/*
+$(document).ready(function() {
+    var $container = $('#gibbactu');
+    $container.isotope({
+        itemSelector: '.gibbactus'
+    });
 });
+*/
