8cf049ea23f5a39499a5b61de74323ffb3ea807a
jakwarrior <jakwarrior45@hotmail.com>
2015-05-24 01:05:11 +0200

    mise en place reset password + envoi mdp par mail

--- app/controllers/AccountController.php
+++ app/controllers/AccountController.php
@@ -66,10 +66,43 @@ class AccountController extends Controller
 
     public function reset()
     {
-        $user = new User($this->db);
-        $user->resetLogin();
-
-        $this->f3->set('view', 'error.html');
+        $this->f3->set('view', 'account/reset.html');
         echo \Template::instance()->render('layout.htm');
+    }
+
+    public function reset_ajax()
+    {
+        $user = new User($this->db);
+
+        $errors = array();
+        $data = array();
+
+        if (!$this->f3->exists('POST.email') || empty($this->f3->get('POST.email')))
+            $errors['email'] = "L'Email est requis";
+
+        if ( !empty($errors)) {
+            $data['success'] = false;
+        } else {
+            $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+            $response = $user->resetLogin($email);
+
+            if ($response == "OK") {
+                $data['success'] = true;
+                $data['message'] = "Le nouveau mot de passe vient de vous être envoyé";
+            }
+            else if ($response == "inconnu") {
+                $data['success'] = false;
+                $data['message'] = 'Le compte correspondant à cette adresse est introuvable';
+                $errors['else'] = 'Le compte correspondant à cette adresse est introuvable';
+            }
+            else if ($response == "problem") {
+                $data['success'] = false;
+                $data['message'] = "Une erreur s'est produite";
+                $errors['else'] = "Une erreur s'est produite";
+            }
+        }
+
+        $data['errors']  = $errors;
+        echo json_encode($data);
     }
 }
--- app/models/User.php
+++ app/models/User.php
@@ -38,25 +38,45 @@ class User extends DB\SQL\Mapper {
         } else {
             return "problem";
         }
-
-
     }
 
-    public function resetLogin()
+    public function resetLogin($email)
     {
-        $password='azerty';
-        $password_hash = hash('sha512', $password);
+        $request = 'SELECT *'.
+            ' FROM user '.
+            ' WHERE email = :email';
 
-        $hash_pwd = password_hash($password_hash, PASSWORD_DEFAULT);
+        $result = $this->db->exec($request, array(':email' => $email));
 
-        $request = 'UPDATE user SET password= :password WHERE hfr_user_id=929138';
+        if (is_array($result) && count($result) == 1)
+        {
+            $newPwd = $this->random_password(12);
+            $request2 = 'UPDATE user SET password= :password WHERE hfr_user_id= :user_id';
+            $result2 = $this->db->exec($request2, array(':password' => $newPwd, ':user_id' => $result[0]['hfr_user_id']));
 
-        return $this->db->exec($request, array(':password' => $hash_pwd));
+            if ($result2 == 1) {
+                $message = html_entity_decode("Bonjour " . html_entity_decode($result[0]['username']) . ",<br><br>Vous avez demandé la réinitialisation de votre mot de passe. Votre nouveau mot de passe est " . $newPwd . "<br><br>A bientôt sur <a href='www.thetartuffebay.org'>TheTartuffeBay.org</a>");
 
-/*        $smtp=new SMTP ($this->f3->get('smtp_server'), $this->f3->get('smtp_port'), false, $this->f3->get('smtp_account'), $this->f3->get('smtp_pwd'));
-        $smtp->set('To', '"plop" <plop@plop.com>');
-        $smtp->set('From', '"The Tartuffe Bay" <no-reply@thetartuffebay.org>');
-        $smtp->set('Subject', 'Sent with the F3 SMTP plug-in');
-        $smtp->send("Plop");*/
+                $smtp=new SMTP ($this->f3->get('smtp_server'), $this->f3->get('smtp_port'), false, $this->f3->get('smtp_account'), $this->f3->get('smtp_pwd'));
+                $smtp->set('Content-Type', 'text/html; charset=UTF-8');
+                $smtp->set('To', $email);
+                $smtp->set('From', '"The Tartuffe Bay" <no-reply@thetartuffebay.org>');
+                $smtp->set('Subject', 'Réinitialisation de votre mot de passe TheTartuffeBay.org');
+                $smtp->send($message);
+
+                return "OK";
+            } else {
+                return "problem";
+            }
+        }
+        else {
+            return "problem";
+        }
+    }
+
+    function random_password( $length = 8 ) {
+        $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+;:,.?";
+        $password = substr( str_shuffle( $chars ), 0, $length );
+        return $password;
     }
 }
