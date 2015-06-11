bd25ebc452f820b817da15ac0d0db7275b53e473
jakwarrior <jakwarrior45@hotmail.com>
2015-06-11 22:53:19 +0200

    fin dev profils

--- app/controllers/ProfilController.php
+++ app/controllers/ProfilController.php
@@ -8,9 +8,16 @@ class ProfilController extends Controller {
         $this->f3->set('list_cr', $myCr = $CR->byUserName($this->f3->get('PARAMS.name')));
 
         $Stream = new Stream($this->db);
-        $this->f3->set('list_stream', $myStream = $Stream->byUserName($this->f3->get('PARAMS.name')));
 
-        if (!count($myCr) && !count($myStream) ) {
+        $streamer = $Stream->byUserName($this->f3->get('PARAMS.name'));
+
+        if (count($streamer) == 1) {
+            $this->f3->set('streamer', $streamer[0]);
+        } else {
+            $this->f3->set('streamer', null);
+        }
+
+        if (!count($myCr) && !count($streamer) ) {
           $this->f3->error(404);
           //$this->f3->reroute('@home');
         }
